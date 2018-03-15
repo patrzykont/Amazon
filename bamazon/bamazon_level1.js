79a1f9d766b345efdbd4b7b450fcc8e062b16af7
patrzykont <aaronpatrzykont@Aarons-MacBook-Air.local>
2018-03-14 23:45:17 -0500

    message

---
+++ bamazon/bamazon_level1.js
@@ -0,0 +1,96 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+require("console.table");
+
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "",
+  	database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+  }
+  loadProducts();
+});
+
+function loadProducts() {
+	connection.query("SELECT * FROM products", function(err, res) {
+    	if (err) throw err;
+    		console.table(res);
+    	promptCustomerForItem(res);
+  });
+}
+
+function promptCustomerForQuantity(product) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "quantity",
+        message: "How many would you like? [Quit with Q]",
+        validate: function(val) {
+          return val > 0 || val.toLowerCase() === "q";
+        }
+      }
+    ])
+    
+    .then(function(val) {
+      checkIfShouldExit(val.quantity);
+      
+      var quantity = parseInt(val.quantity);
+            if (quantity > product.stock_quantity) {
+        console.log("\nInsufficient quantity!");
+        loadProducts();
+      }
+      
+      else { makePurchase(product, quantity);
+      }
+    });
+}
+
+function makePurchase(product, quantity) {
+  connection.query(
+    "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
+    [quantity, product.item_id],
+    
+    function(err, res) {
+    	  console.log("\nSuccessfully purchased " + quantity + " " + product.product_name + "'s!");
+
+      loadProducts();
+    }
+  );
+}
+
+function checkInventory(choiceId, inventory) {
+  
+ 	for (var i = 0; i < inventory.length; i++) {
+    
+    if (inventory[i].item_id === choiceId) {
+			return inventory[i];
+    }
+  }
+  return null;
+}
+
+function checkIfShouldExit(choice) {
+  if (choice.toLowerCase() === "q") {
+
+    console.log("Goodbye!");
+    process.exit(0);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
